d2d11cd8d81f29c9ba82fa4609574caa96400c8f
Antonio Vitor <nP_vitor@hotmail.com>
2025-08-21 17:55:16 -0300

    .

---
+++ Model/SchedulerEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorSimpleScheduler.Model
+{
+    public class SchedulerEvents
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Model/Schedulers.cs
@@ -0,0 +1,14 @@
+﻿namespace MudBlazorSimpleScheduler.Model
+{
+    public class Schedulers
+    {
+        public TimeOnly Time { get; set; }
+        public SchedulerEvents DayOne { get; set; } = new();
+        public SchedulerEvents DayTwo { get; set; } = new();
+        public SchedulerEvents DayThree { get; set; } = new();
+        public SchedulerEvents DayFour { get; set; } = new();
+        public SchedulerEvents DayFive { get; set; } = new();
+        public SchedulerEvents DaySix { get; set; } = new();
+        public SchedulerEvents DaySeven { get; set; } = new();
+    }
+}
